58e25effb33ff62c329fac6a6451d6f723033e95
Rodrigo Ignacio Zenteno Smith <rzenteno@mmlv.cl>
2023-11-22 16:46:19 -0300

    fix static files

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolveSoa } from 'dns';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -10,11 +11,12 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('identidad');
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  //app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use('/identidad', express.static('public'))
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  app.use(express.json({ limit: '25mb' }));
-  app.use(express.urlencoded({ limit: '25mb', extended: true }));
+  app.use(express.json({ limit: '30mb' }));
+  app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
   await app.listen(4500);
 }
